a1a4ca851c418aa28f5251b730132f31ca6ff1e9
Wisaac <l.isaac.taveraz@gmail.com>
2026-01-15 01:00:57 -0600

    Disabling webkit shortcuts

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_prevent_default::PlatformOptions;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +11,23 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(
+            tauri_plugin_prevent_default::Builder::new()
+                .platform(
+                    PlatformOptions::new()
+                        // Whether general form information should be saved and autofilled.
+                        .general_autofill(true)
+                        // Whether password information should be autosaved.
+                        .password_autosave(false)
+                        // Whether browser-specific accelerator keys are enabled.
+                        .browser_accelerator_keys(false)
+                        // Whether the default context menus are shown in the webview.
+                        .default_context_menus(false)
+                        // Whether the webview renders the default JavaScript dialog box.
+                        .default_script_dialogs(true),
+                )
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
